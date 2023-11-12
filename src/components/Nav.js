0cbc12bc87188957827342fb05eb7c9289e8d5a6
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-11-12 18:47:34 -0500

    first draft

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,13 +6,13 @@ export default function Nav() {
   return (
     <>
       <div
-        className={`w-screen h-16 bg-gradient-to-tr from-red-700 to-zinc-600 ${styles.nav}`}
+        className={`w-screen h-16 bg-gradient-to-tr from-red-700 via-amber-700 to-indigo-950 ${styles.nav}`}
       >
         <div className="flex mx-auto p-5 justify-around w-1/2 max-sm:w-full">
           <Link href={"/"}>Home</Link>
-          <Link href={"/"}>Events</Link>
-          <Link href={"/"}>About</Link>
-          <Link href={"/"}>Contact</Link>
+          <Link href={"/events"}>Events</Link>
+          <Link href={"/about"}>About</Link>
+          <Link href={"/contact"}>Contact</Link>
         </div>
       </div>
     </>
